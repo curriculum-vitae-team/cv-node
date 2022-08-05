2243389fb4c032f1398449d783d89cbcf3b8ca49
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-08-05 21:53:23 +0300

    feat: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
@@ -5,8 +6,7 @@ import { AppModule } from "./app/app.module";
 async function start() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 start();
