2b26b7ad9d49b0c056f5d3dbf297cc587f24c8c9
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-08-07 20:08:25 +0300

    fix: playground page cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 
 async function start() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
